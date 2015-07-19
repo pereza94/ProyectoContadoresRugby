a764f9d2ae646d3e98ad759129ce0fd784cf1910
Alejandro E Pérez <perezalejandroes@gmail.com>
2015-07-19 16:35:32 -0300

    Commits iniciales

---
+++ app/src/main/java/com/holamundo/alejandro/alejandro123/GlobalVarables.java
@@ -0,0 +1,223 @@
+package com.holamundo.alejandro.alejandro123;
+
+public class GlobalVarables {
+    private static Integer contpenales = 0;
+    private static Integer contpenalesCargaSinPelota = 0;
+    private static Integer contpenalesDerrumbeDeMaul = 0;
+    private static Integer contpenalesEntrarDeCostado = 0;
+    private static Integer contpenalesInconducta = 0;
+    private static Integer contpenalesLine = 0;
+    private static Integer contpenalesManoEnElRuck = 0;
+    private static Integer contpenalesOffSide = 0;
+    private static Integer contpenalesScrum = 0;
+    private static Integer contpenalesTackleAlto = 0;
+    private static Integer contpenalesTirarseEnElRuck = 0;
+    private static Integer contpenaleReteners = 0;
+    private static Integer contpenalesV = 0;
+    private static Integer contpenalesCargaSinPelotaV = 0;
+    private static Integer contpenalesDerrumbeDeMaulV = 0;
+    private static Integer contpenalesEntrarDeCostadoV = 0;
+    private static Integer contpenalesInconductaV = 0;
+    private static Integer contpenalesLineV = 0;
+    private static Integer contpenalesManoEnElRuckV = 0;
+    private static Integer contpenalesOffSideV = 0;
+    private static Integer contpenalesScrumV = 0;
+    private static Integer contpenalesTackleAltoV = 0;
+    private static Integer contpenalesTirarseEnElRuckV = 0;
+    private static Integer contpenaleRetenersV = 0;
+
+
+    public Integer getContpenalesV() {
+        return contpenalesV;
+    }
+
+    public void setContpenalesV(Integer contpenalesV) {
+        GlobalVarables.contpenalesV = contpenalesV;
+    }
+
+    public Integer getContpenalesCargaSinPelotaV() {
+        return contpenalesCargaSinPelotaV;
+    }
+
+    public void setContpenalesCargaSinPelotaV(Integer contpenalesCargaSinPelotaV) {
+        GlobalVarables.contpenalesCargaSinPelotaV = contpenalesCargaSinPelotaV;
+    }
+
+    public Integer getContpenalesDerrumbeDeMaulV() {
+        return contpenalesDerrumbeDeMaulV;
+    }
+
+    public void setContpenalesDerrumbeDeMaulV(Integer contpenalesDerrumbeDeMaulV) {
+        GlobalVarables.contpenalesDerrumbeDeMaulV = contpenalesDerrumbeDeMaulV;
+    }
+
+    public Integer getContpenalesEntrarDeCostadoV() {
+        return contpenalesEntrarDeCostadoV;
+    }
+
+    public void setContpenalesEntrarDeCostadoV(Integer contpenalesEntrarDeCostadoV) {
+        GlobalVarables.contpenalesEntrarDeCostadoV = contpenalesEntrarDeCostadoV;
+    }
+
+    public Integer getContpenalesInconductaV() {
+        return contpenalesInconductaV;
+    }
+
+    public void setContpenalesInconductaV(Integer contpenalesInconductaV) {
+        GlobalVarables.contpenalesInconductaV = contpenalesInconductaV;
+    }
+
+    public Integer getContpenalesLineV() {
+        return contpenalesLineV;
+    }
+
+    public void setContpenalesLineV(Integer contpenalesLineV) {
+        GlobalVarables.contpenalesLineV = contpenalesLineV;
+    }
+
+    public Integer getContpenalesManoEnElRuckV() {
+        return contpenalesManoEnElRuckV;
+    }
+
+    public void setContpenalesManoEnElRuckV(Integer contpenalesManoEnElRuckV) {
+        GlobalVarables.contpenalesManoEnElRuckV = contpenalesManoEnElRuckV;
+    }
+
+    public Integer getContpenalesOffSideV() {
+        return contpenalesOffSideV;
+    }
+
+    public void setContpenalesOffSideV(Integer contpenalesOffSideV) {
+        GlobalVarables.contpenalesOffSideV = contpenalesOffSideV;
+    }
+
+    public Integer getContpenalesScrumV() {
+        return contpenalesScrumV;
+    }
+
+    public void setContpenalesScrumV(Integer contpenalesScrumV) {
+        GlobalVarables.contpenalesScrumV = contpenalesScrumV;
+    }
+
+    public Integer getContpenalesTackleAltoV() {
+        return contpenalesTackleAltoV;
+    }
+
+    public void setContpenalesTackleAltoV(Integer contpenalesTackleAltoV) {
+        GlobalVarables.contpenalesTackleAltoV = contpenalesTackleAltoV;
+    }
+
+    public Integer getContpenalesTirarseEnElRuckV() {
+        return contpenalesTirarseEnElRuckV;
+    }
+
+    public void setContpenalesTirarseEnElRuckV(Integer contpenalesTirarseEnElRuckV) {
+        GlobalVarables.contpenalesTirarseEnElRuckV = contpenalesTirarseEnElRuckV;
+    }
+
+    public Integer getContpenaleRetenersV() {
+        return contpenaleRetenersV;
+    }
+
+    public void setContpenaleRetenersV(Integer contpenaleRetenersV) {
+        GlobalVarables.contpenaleRetenersV = contpenaleRetenersV;
+    }
+
+
+    public Integer getContpenales() {
+        return contpenales;
+    }
+
+    public void setContpenales(Integer contpenales) {
+        GlobalVarables.contpenales = contpenales;
+    }
+
+    public Integer getContpenalesCargaSinPelota() {
+        return contpenalesCargaSinPelota;
+    }
+
+    public void setContpenalesCargaSinPelota(Integer contpenalesCargaSinPelota) {
+        GlobalVarables.contpenalesCargaSinPelota = contpenalesCargaSinPelota;
+    }
+
+    public Integer getContpenalesDerrumbeDeMaul() {
+        return contpenalesDerrumbeDeMaul;
+    }
+
+    public void setContpenalesDerrumbeDeMaul(Integer contpenalesDerrumbeDeMaul) {
+        GlobalVarables.contpenalesDerrumbeDeMaul = contpenalesDerrumbeDeMaul;
+    }
+
+    public Integer getContpenalesEntrarDeCostado() {
+        return contpenalesEntrarDeCostado;
+    }
+
+    public void setContpenalesEntrarDeCostado(Integer contpenalesEntrarDeCostado) {
+        GlobalVarables.contpenalesEntrarDeCostado = contpenalesEntrarDeCostado;
+    }
+
+    public Integer getContpenalesInconducta() {
+        return contpenalesInconducta;
+    }
+
+    public void setContpenalesInconducta(Integer contpenalesInconducta) {
+        GlobalVarables.contpenalesInconducta = contpenalesInconducta;
+    }
+
+    public Integer getContpenalesLine() {
+        return contpenalesLine;
+    }
+
+    public void setContpenalesLine(Integer contpenalesLine) {
+        GlobalVarables.contpenalesLine = contpenalesLine;
+    }
+
+    public Integer getContpenalesManoEnElRuck() {
+        return contpenalesManoEnElRuck;
+    }
+
+    public void setContpenalesManoEnElRuck(Integer contpenalesManoEnElRuck) {
+        GlobalVarables.contpenalesManoEnElRuck = contpenalesManoEnElRuck;
+    }
+
+    public Integer getContpenalesOffSide() {
+        return contpenalesOffSide;
+    }
+
+    public void setContpenalesOffSide(Integer contpenalesOffSide) {
+        GlobalVarables.contpenalesOffSide = contpenalesOffSide;
+    }
+
+    public Integer getContpenalesScrum() {
+        return contpenalesScrum;
+    }
+
+    public void setContpenalesScrum(Integer contpenalesScrum) {
+        GlobalVarables.contpenalesScrum = contpenalesScrum;
+    }
+
+    public Integer getContpenalesTackleAlto() {
+        return contpenalesTackleAlto;
+    }
+
+    public void setContpenalesTackleAlto(Integer contpenalesTackleAlto) {
+        GlobalVarables.contpenalesTackleAlto = contpenalesTackleAlto;
+    }
+
+    public Integer getContpenalesTirarseEnElRuck() {
+        return contpenalesTirarseEnElRuck;
+    }
+
+    public void setContpenalesTirarseEnElRuck(Integer contpenalesTirarseEnElRuck) {
+        GlobalVarables.contpenalesTirarseEnElRuck = contpenalesTirarseEnElRuck;
+    }
+
+    public Integer getContpenaleReteners() {
+        return contpenaleReteners;
+    }
+
+    public void setContpenaleReteners(Integer contpenaleReteners) {
+        GlobalVarables.contpenaleReteners = contpenaleReteners;
+    }
+    
+}
