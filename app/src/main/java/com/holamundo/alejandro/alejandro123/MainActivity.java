1f664a6a056500c8fda0fe8fab3cebd442d2d45d
Alejandro E Pérez <perezalejandroes@gmail.com>
2015-07-19 16:52:58 -0300

    Commits iniciales

--- app/src/main/java/com/holamundo/alejandro/alejandro123/MainActivity.java
+++ app/src/main/java/com/holamundo/alejandro/alejandro123/MainActivity.java
@@ -1,10 +1,12 @@
 package com.holamundo.alejandro.alejandro123;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity{
@@ -13,6 +15,12 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btPenal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PenaltysActivity.class));
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/holamundo/alejandro/alejandro123/PenaltysActivity.java
+++ app/src/main/java/com/holamundo/alejandro/alejandro123/PenaltysActivity.java
@@ -5,15 +5,144 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 
 public class PenaltysActivity extends Activity{
 
-    @Override
+    private Button btsumar;
+    private Button btsumarV;
+    private TextView ctpenal;
+    private TextView txcantPenales;
+    private TextView txcantPenalesV;
+    private RadioButton rbCargaSinPelota;
+    private RadioButton rbDerrumbeMaul;
+    private RadioButton rbEntradaCostado;
+    private RadioButton rbInconducta;
+    private RadioButton rbLine;
+    private RadioButton rbManos;
+    private RadioButton rbOffSide;
+    private RadioButton rbScrum;
+    private RadioButton rbTackleAlto;
+    private RadioButton rbRetener;
+    private RadioButton rbTirarseEnElRuck;
+
+
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final GlobalVarables g = new GlobalVarables();
         setContentView(R.layout.activity_penaltys);
+        final TextView txcantPenales = (TextView) findViewById(R.id.txContPenales);
+        final TextView txcantPenalesV = (TextView) findViewById(R.id.txContPenalesV);
+        btsumar = (Button) findViewById(R.id.btsumarPenal);
+        rbCargaSinPelota = (RadioButton) findViewById(R.id.rbCargaSinPelota);
+        rbDerrumbeMaul = (RadioButton) findViewById(R.id.brDerrumbe);
+        rbEntradaCostado = (RadioButton) findViewById(R.id.rbEntradaDeCostado);
+        rbInconducta = (RadioButton) findViewById(R.id.rbInconducta);
+        rbLine = (RadioButton) findViewById(R.id.rbLine);
+        rbManos = (RadioButton) findViewById(R.id.rbManos);
+        rbOffSide = (RadioButton) findViewById(R.id.rbOffSide);
+        rbScrum = (RadioButton) findViewById(R.id.rbScrum);
+        rbTackleAlto = (RadioButton) findViewById(R.id.rbTackleAlto);
+        rbRetener = (RadioButton) findViewById(R.id.rbRetener);
+        rbTirarseEnElRuck = (RadioButton) findViewById(R.id.rbTirarseEnElRuck);
+        txcantPenales.setText(String.valueOf(g.getContpenales()));
+        txcantPenalesV.setText(String.valueOf(g.getContpenalesV()));
+        btsumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ;
+                g.setContpenales(g.getContpenales() + 1);
+                if (rbCargaSinPelota.isChecked()) {
+                    g.setContpenalesCargaSinPelota(g.getContpenalesCargaSinPelota() + 1);
+                }
+                if (rbDerrumbeMaul.isChecked()) {
+                    g.setContpenalesDerrumbeDeMaul(g.getContpenalesDerrumbeDeMaul() + 1);
+                }
+                if (rbEntradaCostado.isChecked()) {
+                    g.setContpenalesEntrarDeCostado(g.getContpenalesEntrarDeCostado() + 1);
+                }
+                if (rbInconducta.isChecked()) {
+                    g.setContpenalesInconducta(g.getContpenalesInconducta() + 1);
+                }
+                if (rbLine.isChecked()) {
+                    g.setContpenalesLine(g.getContpenalesLine() + 1);
+                }
+                if (rbManos.isChecked()) {
+                    g.setContpenalesManoEnElRuck(g.getContpenalesManoEnElRuck() + 1);
+                }
+                if (rbOffSide.isChecked()) {
+                    g.setContpenalesOffSide(g.getContpenalesOffSide() + 1);
+                }
+                if (rbScrum.isChecked()) {
+                    g.setContpenalesScrum(g.getContpenalesScrum() + 1);
+                }
+                if (rbTackleAlto.isChecked()) {
+                    g.setContpenalesTackleAlto(g.getContpenalesTackleAlto() + 1);
+                }
+                if (rbRetener.isChecked()) {
+                    g.setContpenaleReteners(g.getContpenaleReteners() + 1);
+                }
+                if (rbTirarseEnElRuck.isChecked()) {
+                    g.setContpenalesTirarseEnElRuck(g.getContpenalesTirarseEnElRuck() + 1);
+                }
+
+                txcantPenales.setText(String.valueOf(g.getContpenales()));
+
+            }
+        });
+        btsumarV = (Button) findViewById(R.id.btSumarV);
+        btsumarV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                g.setContpenalesV(g.getContpenalesV() + 1);
+                if (rbCargaSinPelota.isChecked()) {
+                    g.setContpenalesCargaSinPelotaV(g.getContpenalesCargaSinPelotaV() + 1);
+                }
+                if (rbDerrumbeMaul.isChecked()) {
+                    g.setContpenalesDerrumbeDeMaulV(g.getContpenalesDerrumbeDeMaulV() + 1);
+                }
+                if (rbEntradaCostado.isChecked()) {
+                    g.setContpenalesEntrarDeCostadoV(g.getContpenalesEntrarDeCostadoV() + 1);
+                }
+                if (rbInconducta.isChecked()) {
+                    g.setContpenalesInconductaV(g.getContpenalesInconductaV() + 1);
+                }
+                if (rbLine.isChecked()) {
+                    g.setContpenalesLineV(g.getContpenalesLineV() + 1);
+                }
+                if (rbManos.isChecked()) {
+                    g.setContpenalesManoEnElRuckV(g.getContpenalesManoEnElRuckV() + 1);
+                }
+                if (rbOffSide.isChecked()) {
+                    g.setContpenalesOffSideV(g.getContpenalesOffSideV() + 1);
+                }
+                if (rbScrum.isChecked()) {
+                    g.setContpenalesScrumV(g.getContpenalesScrumV() + 1);
+                }
+                if (rbTackleAlto.isChecked()) {
+                    g.setContpenalesTackleAltoV(g.getContpenalesTackleAltoV() + 1);
+                }
+                if (rbRetener.isChecked()) {
+                    g.setContpenaleRetenersV(g.getContpenaleRetenersV() + 1);
+                }
+                if (rbTirarseEnElRuck.isChecked()) {
+                    g.setContpenalesTirarseEnElRuckV(g.getContpenalesTirarseEnElRuckV() + 1);
+                }
+
+                txcantPenalesV.setText(String.valueOf(g.getContpenalesV()));
+            }
+        });
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
