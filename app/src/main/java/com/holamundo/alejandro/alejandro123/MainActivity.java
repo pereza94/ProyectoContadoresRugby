ce28e945a7c3ed5ef187cbd9fde5ef618cafbf52
Alejandro E Pérez <perezalejandroes@gmail.com>
2015-07-23 15:51:25 -0300

    Andan penales y Free Kicks

--- app/src/main/java/com/holamundo/alejandro/alejandro123/MainActivity.java
+++ app/src/main/java/com/holamundo/alejandro/alejandro123/MainActivity.java
@@ -27,6 +27,12 @@ public class MainActivity extends Activity{
                 startActivity(new Intent(MainActivity.this, Activity_freeKicks.class));
             }
         });
+        findViewById(R.id.btScrum).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScrumActivity.class));
+            }
+        });
     }
 
     @Override
